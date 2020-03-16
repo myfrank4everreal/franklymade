e0cb8871a31169fc6efbd13adc125b5794113d82
myfrank4everreal <myfrank4everrel@gmail.com>
2020-03-16 07:08:50 +0400

    flf

--- sitetemplate/settings.py
+++ sitetemplate/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z@kz)6fktuzryqa4%iw-_*ky4=!jx^t=aifj=pjx&a)*fb$&i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
